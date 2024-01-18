fb02849f8f50b844f8fe17a27bc86c2b5d7c091d
pheno <testuniversity@yandex.ru>
2024-01-19 01:07:25 +0300

    1. updated "Readme.md" 2. updated ".gitignore" 3. added 2 stubs function to the "playlist_analyzer.h" 4. renamed struct from "play_list_link" to "play_list_path" in the "playlist_analyzer.h" 5. refactored "playlist_analyzer.cpp" 6. added 2 function to "main.cpp" 7. added flag "valid_choice" for while loop 8. added msgs to "main.cpp"

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 int main()
 {
     size_t choice;
+    bool valid_choice = false;
     
-    while (true)
+    while (!valid_choice)
     {
         std::cout << "Выберите действие:\n"
                   << "1. Анализ сломанных путей до файла.\n"
@@ -15,10 +16,14 @@ int main()
                      "Если все файлы находятся в одной папке, то заменяет путь до папки.\n"
                   << "3. Умная замена путей до файла.\n"
                      "Ищет по названиям файлов в директории N и в подпапках этой директории, меняет путь.\n"
-                  << "4. Другая функциональность.\n";
+                  << "4. Проверка двух файлов XSPF на различия.\n"
+                     "Программа может сравнивать два файла XSPF и находить различия между ними.\n"
+                  << "5. Создать новый файл с невстречающимися файлами после сравнения.\n"
+                     "Будет создан новый файл XSPF, который будет содержать файлы, "
+                     "присутствующие в одной версии, но отсутствующие в другой.\n";
         
         
-        std::cout << "Выш выбор: ";
+        std::cout << std::endl << "Выберите один из вариантов (1-5): ";
         if (!(std::cin >> choice) || choice < 1 || choice > 5)
         {
             std::cout << "Неправильный выбор. Попробуйте снова.\n" << std::endl;
@@ -28,25 +33,35 @@ int main()
         else
         {
             // Ввод корректный, выходим из цикла
-            break;
+            valid_choice = true;
         }
     }
     
+    //std::cout << "Выш выбор: " << choice << std::endl;
     switch (choice)
     {
         case 1:
-            std::cout << "Вы выбрали анализ плейлиста.\n";
+            std::cout << "Вы выбрали " << choice << " \"анализ плейлиста\".\n";
             analyze_playlist(PLAY_LIST_PATH);
             break;
         case 2:
-            std::cout << "Вы выбрали замену пути.\n";
+            std::cout << "Вы выбрали " << choice << " \"простую замену пути\".\n";
             replace_path_simple(PLAY_LIST_PATH, NEW_PATH);
             break;
         case 3:
+            std::cout << "Вы выбрали " << choice << " \"умную замену путей\".\n";
             replace_path_smart(PLAY_LIST_PATH, DIRECTORY_PATH);
             break;
+        case 4:
+            std::cout << "Вы выбрали " << choice << " \"проверку 2х файлов на различия\".\n";
+            checkin_xspf_diff(PLAY_LIST_PATH);
+            break;
+        case 5:
+            std::cout << "Вы выбрали " << choice <<  " \"создание нового файла с различиями\".\n";
+            create_new_file_with_diff(PLAY_LIST_PATH);
+            break;
         default:
-            std::cout << "Неправильный выбор.\n";
+            std::cout << "Неправильный выбор!\n";
             return 1;
     }
     
