0de89ab4fe4dcb78ccce9d7982a8707e231abf7c
pheno <testuniversity@yandex.ru>
2024-01-20 03:47:42 +0300

    1. updated "Readme.md" 2. added new file in ".gitignore" 3. added comments and 2 constants XSPF_DIFF_PATH and SECOND_PLAY_LIST_PATH to the file "config_example.h" 4. added comment and variables for function "checkin_xspf_diff" 5. updated "main.cpp" 6. added function "checkin_xspf_diff" and static functions "get_filename, read_playlist"

--- config_example.h
+++ config_example.h
@@ -1,8 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// Пути до текстовых файлов .txt
 const auto SAVED_FILE_PATH = "/path/to/broken_links/file/broken_links.txt";
+const auto XSPF_DIFF_PATH = "/path/to/broken_links/file/xspf_diff.txt";
+
+// Пути до файлов .xspf
 const auto PLAY_LIST_PATH = "/path/to/playlist/file/playlist.xspf";
+const auto SECOND_PLAY_LIST_PATH = "/path/to/playlist/file/playlist2.xspf";
+
+// Пути до папок
 const auto NEW_PATH = "/new_path/to/file";
 const auto DIRECTORY_PATH = "/directory_path/to/file"
 
